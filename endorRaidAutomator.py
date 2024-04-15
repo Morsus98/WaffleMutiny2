7190822ef0f48c8a96e2e7d75dc8e287f1fc9ef5
jclarse2 <jclarse2@ncsu.edu>
2024-04-14 20:18:38 -0400

    Added restart mechanic
    Now the system restarts after failing

--- endorRaidAutomator.py
+++ endorRaidAutomator.py
@@ -14,39 +14,42 @@ import cv2
 # The moves have the following priority: Evasive Maneuvers, Hasty Repairs, Forest Friends, and Basic Attack.
 # The script will not count a turn if it is unable to find a move to make.
 
-maximum_turns = 50
-i = 0
-EM_stacks = 0
-# Initializing the while loop for turn count
-while i < maximum_turns:
-    # Check for the available moves - note that evasive maneuvers may not exactly match the image
-    basic_attack = pyautogui.locateOnScreen('basic_attack.png', confidence=0.85)
-    forest_friends = pyautogui.locateOnScreen('forest_friends.png', confidence=0.85)
-    hasty_repairs = pyautogui.locateOnScreen('hasty_repairs.png', confidence=0.85)
-    evasive_maneuvers = pyautogui.locateOnScreen('evasive_maneuvers.png', confidence=0.85)
-    if evasive_maneuvers is not None and EM_stacks < 65:
-        print("Evasive Maneuvers found")
-        pyautogui.click(evasive_maneuvers)
-        EM_stacks += 4
-        i += 1
-    elif hasty_repairs is not None:
-        print("Hasty Repairs found")
-        pyautogui.click(hasty_repairs)
-        #EM_stacks += 3
-        i += 1
-    elif forest_friends is not None:
-        print("Forest Friends found")
-        pyautogui.click(forest_friends)
-        #EM_stacks += 2
-        i += 1
-    elif basic_attack is not None:
-        print("Basic Attack found")
-        pyautogui.click(basic_attack)
-        i += 1
-    else:
-        print("No move found")
-    # Wait 2 seconds for the next turn to finish loading
-    time.sleep(1)
+def attempt_the_raid():
+    maximum_EM = 16
+    i = 0
+    EM_stacks = 0
+    EM_Uses = 0
+    # Initializing the while loop for turn count
+    while EM_Uses < maximum_EM:
+        # Check for the available moves - note that evasive maneuvers may not exactly match the image
+        basic_attack = pyautogui.locateOnScreen('basic_attack.png', confidence=0.85)
+        forest_friends = pyautogui.locateOnScreen('forest_friends.png', confidence=0.85)
+        hasty_repairs = pyautogui.locateOnScreen('hasty_repairs.png', confidence=0.85)
+        evasive_maneuvers = pyautogui.locateOnScreen('evasive_maneuvers.png', confidence=0.85)
+        if evasive_maneuvers is not None and EM_stacks < 65:
+            print("Evasive Maneuvers found")
+            pyautogui.click(evasive_maneuvers)
+            EM_stacks += 4
+            EM_Uses += 1
+            i += 1
+        elif hasty_repairs is not None:
+            print("Hasty Repairs found")
+            pyautogui.click(hasty_repairs)
+            EM_stacks += 3
+            i += 1
+        elif forest_friends is not None:
+            print("Forest Friends found")
+            pyautogui.click(forest_friends)
+            EM_stacks += 2
+            i += 1
+        elif basic_attack is not None:
+            print("Basic Attack found")
+            pyautogui.click(basic_attack)
+            i += 1
+        else:
+            print("No move found")
+        # Wait 2 seconds for the next turn to finish loading
+        time.sleep(1)
 
 # I want a new function to restart if the run is not going well to minimize the time spent on a bad run.
 def start_over():
@@ -61,19 +64,19 @@ def start_over():
     # We confirm the retreat
     confirm = pyautogui.locateOnScreen('YesButton.png', confidence=0.85)
     pyautogui.click(confirm)
-    time.sleep(2)
+    time.sleep(10)
     # Click the Battle (#)
     battle = pyautogui.locateOnScreen('BattleButton.png', confidence=0.85)
     pyautogui.click(battle)
-    time.sleep(2)
-    # Click the Leia icon to remove her button
+    time.sleep(5)
+    # Click the Leia icon to remove her button - here confidence is lower since she's in motion
     try:
-        leia = pyautogui.locateOnScreen('LeiaIcon.png', confidence=0.85)
+        leia = pyautogui.locateOnScreen('LeiaIcon.png', confidence=0.60)
         pyautogui.click(leia)
         time.sleep(2)
     except:
         pass
-    # Click the Battle 2 icon to start the raid
+    # Click the Battle 2 icon
     battle2 = pyautogui.locateOnScreen('BattleIcon2.png', confidence=0.85)
     pyautogui.click(battle2)
     time.sleep(2)
@@ -84,6 +87,32 @@ def start_over():
         time.sleep(2)
         # We'll need to click battle2 again
         battle2 = pyautogui.locateOnScreen('BattleIcon2.png', confidence=0.85)
+        pyautogui.click(battle2)
     except:
         pass
 
+def check_for_minimum_evasion():
+    # If we have not met the minimum EM stacks, we will restart the run
+    # We'll check by looking for the EM icon, which has a number in the top right corner
+    # We'll only need the first digit of the number to determine if we have the minimum stacks
+    # The icons to check for are saved as FiftyEvasion.png, FortyEvasion.png, ThirtyEvasion.png
+    # If either of these icons are found, we will restart the run - we'll want high confidence for this check
+    fifty_evasion = pyautogui.locateOnScreen('FiftyEvasion.png', confidence=0.9)
+    forty_evasion = pyautogui.locateOnScreen('FortyEvasion.png', confidence=0.9)
+    thirty_evasion = pyautogui.locateOnScreen('ThirtyEvasion.png', confidence=0.9)
+    if fifty_evasion is not None or forty_evasion is not None or thirty_evasion is not None:
+        print("Restarting the run")
+        return False
+    else:
+        print("EM stacks are good, returning control to the player")
+        return True
+    
+ready_for_manual_play = False
+while not ready_for_manual_play:
+    attempt_the_raid()
+    ready_for_manual_play = check_for_minimum_evasion()
+    if not ready_for_manual_play:
+        start_over()
+    else:
+        print("Ready for manual play")
+        break
